fc6e77a85744f6effdd22a7c76a68c36304fc691
lmProgramming <50277266+lmProgramming@users.noreply.github.com>
2023-05-22 10:28:54 +0200

    Add files via upload

--- DisjointSetForest.java
+++ DisjointSetForest.java
@@ -1,15 +1,93 @@
 public class DisjointSetForest implements IDisjointSetStructure
 {
-    public DisjointSetForest(int size) {
+    private final Node[] nodes;
+
+    public DisjointSetForest(int size)
+    {
+        nodes = new Node[size];
+        for (int i = 0; i < size; i++)
+        {
+            nodes[i] = new Node(i);
+        }
     }
 
     @Override
     public int findSet(int item) throws ItemOutOfRangeException {
-        return 0;
+        validateItem(item);
+
+        Node node = nodes[item];
+
+        return findNode(node).item;
+    }
+
+    public Node findNode(int item) throws ItemOutOfRangeException
+    {
+        validateItem(item);
+
+        Node node = nodes[item];
+
+        return findNode(node);
+    }
+
+    public Node findNode(Node node)
+    {
+        if (node.parent != node)
+        {
+            node.parent = findNode(node.parent);
+        }
+        return node.parent;
     }
 
     @Override
     public void union(int item1, int item2) throws ItemOutOfRangeException {
+        validateItem(item1);
+        validateItem(item2);
 
+        if (item1 == item2)
+        {
+            return;
+        }
+
+        Node root1 = findNode(item1);
+        Node root2 = findNode(item2);
+
+        link(root1, root2);
+    }
+
+    public void link(Node root1, Node root2)
+    {
+        if (root1.rank < root2.rank)
+        {
+            root1.parent = root2;
+        }
+        else
+        {
+            root2.parent = root1;
+
+            if (root1.rank == root2.rank)
+            {
+                root1.rank++;
+            }
+        }
+    }
+
+    private void validateItem(int item) throws ItemOutOfRangeException
+    {
+        if (item < 0 || item >= nodes.length)
+        {
+            throw new ItemOutOfRangeException();
+        }
+    }
+
+    private static class Node {
+        int item;
+        Node parent;
+        int rank;
+
+        public Node(int item) {
+            this.item = item;
+            this.parent = this;
+            this.rank = 0;
+        }
     }
 }
--- DisjointSetLinkedList.java
+++ DisjointSetLinkedList.java
@@ -1,5 +1,5 @@
 public class DisjointSetLinkedList implements IDisjointSetStructure {
-    private Node[] nodes;
+    private final Node[] nodes;
 
     public DisjointSetLinkedList(int size) {
         nodes = new Node[size];
@@ -18,30 +18,40 @@ public class DisjointSetLinkedList implements IDisjointSetStructure {
 
     @Override
     public int findSet(int item) throws ItemOutOfRangeException {
-        if (item < 0 || item >= nodes.length) {
+        validateItem(item);
+
+        Node node = nodes[item];
+
+        return findRepresentative(node).value;
+    }
+
+    private void validateItem(int item) throws ItemOutOfRangeException
+    {
+        if (item < 0 || item >= nodes.length)
+        {
             throw new ItemOutOfRangeException();
         }
-        Node node = nodes[item];
-        return findRepresentative(node).value;
     }
 
     @Override
     public void union(int item1, int item2) throws ItemOutOfRangeException {
-        if (item1 < 0 || item1 >= nodes.length || item2 < 0 || item2 >= nodes.length) {
-            throw new ItemOutOfRangeException();
-        }
+        validateItem(item1);
+        validateItem(item2);
+
         Node node1 = nodes[item1];
         Node node2 = nodes[item2];
 
         Node representative1 = findRepresentative(node1);
         Node representative2 = findRepresentative(node2);
 
-        if (representative1 == representative2) {
+        if (representative1 == representative2)
+        {
             return; // Both items are already in the same set
         }
 
         if (representative1.length < representative2.length) {
             Node temp = representative1;
+
             representative1 = representative2;
             representative2 = temp;
         }
@@ -50,5 +60,23 @@ public class DisjointSetLinkedList implements IDisjointSetStructure {
         representative1.last.next = nodes[representative2.value];
         representative1.last = representative2.last;
         representative1.length += representative2.length;
+    }
+
+    private static class Node
+    {
+        int value;
+        Node representative;
+        Node last;
+        int length;
+        Node next;
+
+        Node(int value)
+        {
+            this.value = value;
+            this.representative = this;
+            this.last = this;
+            this.length = 1;
+            this.next = this;
+        }
     }
 }
